307937871a893061ddbe189a40244774c5092da2
Nuethera <vaishnavj2124@outlook.com>
2021-10-14 17:00:32 +0530

    aded highscore tracking

--- score.py
+++ score.py
@@ -6,6 +6,8 @@ FONT = "Verdana"
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        with open('highScore.txt', 'r') as f:
+            self.highscore = int(f.readline())
         self.hideturtle()
         self.up()
         self.color("dark turquoise")
@@ -13,18 +15,24 @@ class Score(Turtle):
         self.welcome()
 
     def __write_score(self):
-        self.write(arg=f"SCORE: {self.user_score} ", move=False, align=ALIGNMENT, font=(FONT, 20, 'normal'))
+        self.clear()
+        self.write(arg=f"SCORE: {self.user_score} HIGHSCORE: {self.highscore}", move=False, align=ALIGNMENT, font=(FONT, 15, 'normal'))
 
     def update_score(self):
         self.user_score += 1
-        self.clear()
+
         self.__write_score()
 
     def game_over(self):
-        self.clear()
+
         self.home()
         self.write(arg=f"GAME OVER!\nYOUR FINAL SCORE IS {self.user_score}\nPress Space to restart or\nClick anywhere to exit",
-                   move=False, align=ALIGNMENT, font=(FONT, 20, 'normal'))
+                   move=False, align=ALIGNMENT, font=(FONT, 15, 'normal'))
+        if self.user_score > self.highscore:
+            self.highscore = self.user_score
+            with open('highScore.txt','w') as f:
+                f.write(str(self.highscore))
+
         self.goto(0, 270)
 
     def welcome(self):
